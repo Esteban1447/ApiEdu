688c615b9421d70fa8a4cebccf311b86efc0c62d
Esteban1447 <esteban.robledo.rc@gmail.com>
2025-06-12 00:31:23 -0500

    Creacion de cors 3

--- src/main/java/com/example/APIClassRoom/Config/CorsConfig.java
+++ src/main/java/com/example/APIClassRoom/Config/CorsConfig.java
@@ -1,3 +1,5 @@
+package com.example.APIClassRoom.Config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -20,3 +22,4 @@ public class CorsConfig {
         };
     }
 }
+
