9888021fb485c7d18e7ecd01a4cb316c9619fa3e
Esteban1447 <esteban.robledo.rc@gmail.com>
2025-06-12 00:43:01 -0500

    Cors Any

--- src/main/java/com/example/APIClassRoom/Config/CorsConfig.java
+++ src/main/java/com/example/APIClassRoom/Config/CorsConfig.java
@@ -13,12 +13,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("*") // 👈 Permite cualquier origen
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedHeaders("*");
             }
         };
     }
 }
-
